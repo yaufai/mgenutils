62cca1e3ff2ff8295efb9bd15b9fa7170f78bd06
yaufai <youhui.dev@gmail.com>
2023-06-16 18:28:27 +0800

    :wrench: setup packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mgenutils',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author="yaufai",
+    author_email="youhui.dev@gmail.com",
+)
